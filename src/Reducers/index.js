9aab644362ba28769f915638cc59008c34594d6e
mohitgreatwits <42135477+mohitgreatwits@users.noreply.github.com>
2019-01-21 14:17:40 +0530

    index

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,17 +1,12 @@
 import { combineReducers } from 'redux';
-// import {reducer as formReducer} from 'redux-form';
 import userDetail from './registration_reducer';
 import getRoles from './fetchRoles';
 import loginReducer from './Login_reducer';
 import flats from './flatReducer';
 import flat from './flatMasterReducer';
 import societyReducer from './Society_reducer'
-
-// import loginReducer from './login_reducer';
 import TowerDetails from './tower_reducer';
 import SizeDetails from './size_reducer';
-// import SizeDetails from './size_reducer';
-// import TowerDetails from './tower_reducer';
 import EventDetails from './event_reducer';
 
 const rootReducer = combineReducers({
@@ -23,7 +18,6 @@ const rootReducer = combineReducers({
     flats,
     flat,
     EventDetails,
-    flats,
     societyReducer
 })
 export default rootReducer;
