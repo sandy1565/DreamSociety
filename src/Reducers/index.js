92842e54983c6ce66f5d51ff9edf538499f971fe
shubang22 <46522119+shubang22@users.noreply.github.com>
2019-01-23 12:52:58 +0530

    pushed by siddharth

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -3,6 +3,9 @@ import userDetail from './registration_reducer';
 import loginReducer from './Login_reducer';
 import flats from './flatReducer';
 import flat from './flatMasterReducer';
+import TowerDetails from './tower_reducer';
+import SizeDetails from './size_reducer';
+import societyReducer from './Society_reducer';
 import EventDetails from './event_reducer';
 import serviceMasterReducer from './VendorMangement/serviceMasterReducer';
 import displayServiceMasterReducer from './VendorMangement/displayServiceMasterReducer';
@@ -14,7 +17,6 @@ const rootReducer = combineReducers({
     TowerDetails,
     SizeDetails,
     EventDetails,
-    societyReducer,
     flats,
     societyReducer,
     serviceMasterReducer,
