c0ba4a5d88cac3b385b7eaf444a603665e82053e
aakashgaba1 <40385516+aakashgaba1@users.noreply.github.com>
2019-01-23 11:41:20 +0530

    size, tower completed

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -3,6 +3,9 @@ import userDetail from './registration_reducer';
 import loginReducer from './Login_reducer';
 import flats from './flatReducer';
 import flat from './flatMasterReducer';
+
+
+
 import EventDetails from './event_reducer';
 import serviceMasterReducer from './VendorMangement/serviceMasterReducer';
 import displayServiceMasterReducer from './VendorMangement/displayServiceMasterReducer';
@@ -17,7 +20,6 @@ const rootReducer = combineReducers({
     TowerDetails,
     SizeDetails,
     flats,
-    
     EventDetails,
 
     
